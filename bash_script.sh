43d37d9709a38c3c25c72166c3dd1d00ab5422ef
amandinepo <98370509+amandinepo@users.noreply.github.com>
2023-05-11 01:57:37 +0200

    Update bash_script.sh

--- bash_script.sh
+++ bash_script.sh
@@ -5,4 +5,10 @@ sudo apt-get update && sudo apt-get dist-upgrade
 sudo apt install python3-venv
 sudo apt-get install apache2
 
+mkdir flask_app && cd flask_app
+python3 -m venv venv
+source venv/bin/activate
+
+pip install Flask
+
 
