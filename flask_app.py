350f130f14bd63ac057d4fb5734d0cd9cda3a8c8
amandinepo <98370509+amandinepo@users.noreply.github.com>
2023-05-11 02:21:20 +0200

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,8 @@
-from flask import flask
-app = flask(__name__)
+from flask import Flask
+app = Flask(__name__)
 
-@app.route("/")
-def hello():
-  return "Hello world"
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
   
