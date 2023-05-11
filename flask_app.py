8fffad2f3d56e2e9084259bb3178ba16660ca9b5
amandinepo <98370509+amandinepo@users.noreply.github.com>
2023-05-11 21:47:35 +0200

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,5 @@
 from flask import Flask
 app = Flask(__name__)
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
-@app.route('/hello')
-def hello_world():
-    return 'Hello World!'
-
-  
+@app.route("/")
+def helloworld():
+	return 'Hello World!'
