0ca7759e8759499bcf84635638224579b563f775
amandinepo <98370509+amandinepo@users.noreply.github.com>
2023-05-12 18:13:21 +0200

    Update and rename flask_app.py to dash_part.py

---
+++ dash_part.py
@@ -0,0 +1,26 @@
+from dash import dcc,Dash, html, Input, Output,State
+import plotly.express as px
+import pandas as pd
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    html.Div(dcc.Input(id='input-on-submit', type='text')),
+    html.Button('Submit', id='submit-val', n_clicks=0),
+    html.Div(id='container-button-basic',
+             children='Enter a value and press submit')
+])
+
+
+@app.callback(
+    Output('container-button-basic', 'children'),
+    Input('submit-val', 'n_clicks'),
+    State('input-on-submit', 'value')
+)
+def update_output(n_clicks, value):
+    return 'The input value was "{}" and the button has been clicked {} times'.format(
+        value,
+        n_clicks
+)
+
+app.run_server(debug=True)
