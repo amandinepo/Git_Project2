33112c0b168d2bb56c797dae6ae7865835d3ec5b
amandinepo <98370509+amandinepo@users.noreply.github.com>
2023-05-12 12:39:47 +0200

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,24 @@
-from flask import Flask
-app = Flask(__name__)
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+from dash import Dash, html, Input, Output
 
-@app.route("/")
-def helloworld():
-	return 'Hello World!'
+app = Dash(__name__)
+
+app.layout = html.Div(
+    [
+        html.Div(
+            "Div with n_clicks event listener",
+            id="click-div",
+            style={"color": "red", "font-weight": "bold"},
+        ),
+        html.P(id="click-output"),
+    ]
+)
+
+@app.callback(
+    Output("click-output", "children"),
+    Input("click-div", "n_clicks")
+    )
+def click_counter(n_clicks):
+    return f"The html.Div above has been clicked this many times: {n_clicks}"
+
+
+app.run_server(debug=True)
