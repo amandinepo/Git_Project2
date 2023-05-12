58de92fca786535752c83d038eda55074664ee8b
amandinepo <98370509+amandinepo@users.noreply.github.com>
2023-05-12 12:39:22 +0200

    Update bash_script.sh

--- bash_script.sh
+++ bash_script.sh
@@ -5,19 +5,7 @@ sudo apt-get update && sudo apt-get dist-upgrade
 sudo apt install python3-venv
 sudo apt-get install apache2
 
-mkdir flask_app && cd flask_app
-python3 -m venv venv
-source venv/bin/activate
+python3 -m venv .venvs/dash
 
-pip install Flask
-
-git clone https://github.com/amandinepo/Git_Project2.git
-cd Git_Project2
-pip install Flask
-
-sudo systemctl start apache2
-
-export FLASK_APP=hello.py
-flask run --host=0.0.0.0
 
 
