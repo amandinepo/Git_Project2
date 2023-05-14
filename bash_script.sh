0ca98db8ec772028e34a1bf44eedcf8cc914e731
amandinepo <98370509+amandinepo@users.noreply.github.com>
2023-05-14 09:58:23 +0200

    Update bash_script.sh

--- bash_script.sh
+++ bash_script.sh
@@ -4,6 +4,8 @@ sudo apt-get update && sudo apt-get dist-upgrade
 
 sudo apt install python3-venv
 sudo apt-get install apache2
+pip install dash
+pip install pandas
 
 python3 -m venv .venvs/dash
 source .venvs/dash/bin/activate
